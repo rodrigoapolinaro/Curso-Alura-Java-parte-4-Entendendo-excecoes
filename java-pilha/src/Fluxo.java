f8cfdd297935f31698bf195edb0ff06cc1ced680
rodrigoapolinaro <rodrigoapolinaro@icloud.com>
2018-07-06 02:54:54 -0300

    Aula02 - Tratamento de exceções

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -4,7 +4,13 @@ public class Fluxo {
 	
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
-		metodo1();
+		try {
+			metodo1();
+		} catch (ArithmeticException | NullPointerException ex) {
+			String message = ex.getMessage();
+			System.out.println("Exception " + message);
+			ex.printStackTrace();
+		}
 		System.out.println("Fim do main");
 	}
 	
@@ -18,6 +24,9 @@ public class Fluxo {
 		System.out.println("Ini do metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
+			int a = i / 0;
+//			Conta c = null;
+//			c.deposita();
 		}
 		System.out.println("Fim do metodo2");
 	}
